36f7e75b1ff5e71a43bc20e0bb3efae7aae90ab5
kaylin subramanian <kaylinsubramanian@gmail.com>
2026-08-02 22:03:53 +0200

    Extended Words Array for the memorable password creation

--- Java.js
+++ Java.js
@@ -1,53 +1,62 @@
 const passwordWords = [
-    // Animals
-    "Tiger","Lion","Wolf","Fox","Bear","Panther","Leopard","Jaguar","Cheetah","Falcon",
-    "Eagle","Hawk","Owl","Raven","Crow","Shark","Whale","Dolphin","Octopus","Kraken",
-    "Dragon","Phoenix","Griffin","Hydra","Cobra","Viper","Python","Scorpion","Rhino","Bison",
-    "Buffalo","Moose","Stag","Otter","Badger","Wolverine","Lynx","Puma","Koala","Penguin",
-
-    // Nature
-    "Storm","Thunder","Lightning","Rain","Snow","Blizzard","Hurricane","Tornado","Cyclone","Breeze",
-    "River","Ocean","Lake","Waterfall","Mountain","Hill","Valley","Forest","Jungle","Canyon",
-    "Volcano","Glacier","Desert","Savanna","Tundra","Island","Reef","Coral","Pebble","Stone",
-    "Crystal","Ember","Flame","Ash","Smoke","Cloud","Mist","Shadow","Sunrise","Sunset",
-
-    // Space
-    "Galaxy","Nebula","Nova","Comet","Meteor","Asteroid","Orbit","Cosmos","Stellar","Lunar",
-    "Solar","Eclipse","Rocket","Apollo","Saturn","Jupiter","Mars","Venus","Mercury","Pluto",
-    "Quasar","Pulsar","Gravity","Photon","Cosmic","Celestial","Star","Moon","Planet","Aurora",
-
-    // Mythology & History
-    "Zeus","Athena","Apollo","Hermes","Hades","Poseidon","Ares","Artemis","Odin","Thor",
-    "Loki","Freya","Achilles","Odysseus","Leonidas","Spartan","Titan","Atlas","Pegasus","Cerberus",
-    "Minotaur","Cyclops","Medusa","Helios","Perseus","Hercules","Anubis","Ra","Osiris","Valkyrie",
-
-    // Technology
-    "Binary","Cipher","Matrix","Quantum","Pixel","Vector","Kernel","Server","Router","Logic",
-    "Script","Python","Java","Ruby","Cloud","Crypto","Cache","Database","Network","Protocol",
-    "Linux","Windows","Compiler","Git","Terminal","Docker","Code","Syntax","Runtime","Algorithm",
-
-    // Objects
-    "Hammer","Shield","Sword","Arrow","Bow","Spear","Axe","Dagger","Helmet","Armor",
-    "Lantern","Compass","Anchor","Wheel","Engine","Rocket","Beacon","Mirror","Torch","Key",
-    "Lock","Chain","Bridge","Castle","Tower","Crown","Coin","Clock","Book","Scroll",
-
-    // Adjectives
-    "Brave","Silent","Swift","Rapid","Fearless","Hidden","Golden","Silver","Crimson","Emerald",
-    "Ancient","Royal","Wild","Electric","Frozen","Burning","Epic","Stealth","Bold","Lucky",
-    "Mighty","Sharp","Clever","Wise","Noble","Dark","Bright","Iron","Steel","Scarlet",
-
-    // Cities & Places
-    "Tokyo","Paris","London","Berlin","Sydney","Rome","Athens","Dubai","Oslo","Cairo",
-    "Amazon","Everest","Sahara","Arctic","Pacific","Atlantic","Andes","Alps","Olympus","Atlantis",
-    "Babylon","Sparta","Troy","Avalon","Camelot","Eden","Utopia","Nexus","Horizon","Frontier",
-
-    // Gems & Materials
-    "Diamond","Ruby","Emerald","Sapphire","Topaz","Opal","Quartz","Obsidian","Granite","Marble",
-    "Titanium","Carbon","Steel","Bronze","Silver","Gold","Platinum","Copper","Iron","Nickel",
-
-    // Weather & Elements
-    "Frost","Inferno","Tempest","Monsoon","Hail","Drizzle","Gale","Thunderbolt","Spark","Volt",
-    "Blaze","Tsunami","Earthquake","Wildfire","Avalanche","Dust","Steam","Wave","Current","Whirlpool"
+    "Eagle","Fox","Moose","Rhino","Leopard","Cheetah","Badger","Wolverine","Puma","Koala",
+    "Octopus","Kraken","Griffin","Hydra","Viper","Python","Scorpion","Buffalo","Stag","Hawk",
+    "Owl","Crow","Pelican","Swan","Robin","Finch","Heron","Goose","Seal","Walrus",
+    "Savanna","Tundra","Waterfall","Jungle","Reef","Pebble","Mist","Smoke","Ash","Breeze",
+    "Cyclone","Tornado","Blizzard","Snow","Rain","Wave","Current","Whirlpool","Steam","Dust",
+    "Inferno","Tempest","Monsoon","Hail","Drizzle","Gale","Spark","Volt","Wildfire","Avalanche",
+    "Nebula","Asteroid","Cosmos","Lunar","Solar","Orbit","Quasar","Pulsar","Celestial","Cosmic",
+    "Pluto","Apollo","Stellar","Gravity","Planet","Moon","Star","Cosmic","Meteorite","Observatory",
+    "Zeus","Athena","Hermes","Hades","Poseidon","Ares","Artemis","Odin","Thor","Loki",
+    "Freya","Achilles","Odysseus","Leonidas","Spartan","Titan","Atlas","Cerberus","Minotaur","Cyclops",
+    "Medusa","Helios","Perseus","Hercules","Anubis","Osiris","Ra","Valkyrie","Camelot","Babylon",
+    "Kernel","Server","Router","Cache","Linux","Windows","Compiler","Git","Code","Algorithm",
+    "Boolean","Array","Object","Function","Variable","Pointer","Socket","Thread","Process","Package",
+    "Module","Console","Browser","Engine","Framework","Library","Virtual","Machine","Backend","Frontend",
+    "Helmet","Armor","Axe","Dagger","Wheel","Engine","Coin","Book","Locksmith","Backpack",
+    "Notebook","Wallet","Bottle","Keyboard","Monitor","Camera","Pillow","Blanket","Museum","Village",
+    "Canyon","Laptop","Falcon","Honey","Anchor","Meteor","Lantern","Coffee","Tiger","Quartz",
+    "Violin","Pepper","Avalanche","Compass","Jungle","Knight","Blueprint","Cherry","Otter","Photon",
+    "Castle","River","Dragon","Sapphire","Hammer","Vanilla","Galaxy","Shield","Forest","Atom",
+    "Pasta","Volcano","Cobra","Notebook","Rocket","Crystal","Storm","Mango","Python","Bridge",
+    "Emerald","Glacier","Camera","Phoenix","Basil","Saturn","Baker","Tornado","Wolf","Library",
+    "Coral","Neptune","Guitar","Arrow","Pineapple","Matrix","Mountain","Diamond","Telescope","Penguin",
+    "Harbor","Lightning","Cookie","Viking","Safari","Engine","Whale","Topaz","Coconut","Temple",
+    "Scorpion","Canvas","Mercury","Explorer","Marble","Backpack","Moose","Aurora","Submarine","Cinnamon",
+    "Jupiter","Falconer","Bread","Iron","Parrot","Voyager","Lantern","Obsidian","Sailor","Pebble",
+    "Thunder","Lighthouse","Ruby","Dolphin","Castlewall","Kiwi","Compiler","Bison","Garden","Torch",
+    "Star","Cheetah","Bronze","Cactus","Robot","Island","Compass","Blueberry","Captain","Jetstream",
+    "Hydra","Cloud","Pixel","Olive","Scroll","Arctic","Ranger","Firefly","Silver","Trident",
+    "Docker","Lemon","Fox","Breeze","Gold","Astronaut","Canary","Beacon","Cliff","Spear",
+    "Moonlight","Circuit","Knightfall","Maple","Eagle","Vault","Orbit","Waterfall","Chef","Butterfly",
+    "Walnut","Falcon","Lantern","Cucumber","Mercury","Backpack","Otter","Cobalt","Bicycle","Peppermint",
+    "Harpoon","Galaxy","Robin","Cinnamon","Voyage","Anchor","Quartz","Pumpkin","Notebook","Leopard",
+    "Tulip","Whirlpool","Captain","Avocado","Glacier","Helmet","Raccoon","Volcano","Blueprint","Tornado",
+    "Mushroom","Compass","Daisy","Rocket","Hammer","Papaya","Sculpture","Lightning","Coyote","Library",
+    "Chocolate","Saturn","Pebble","Telescope","Maple","Explorer","Cardinal","Waterfall","Pyramid","Olive",
+    "Badger","Sandwich","Jasmine","River","Algorithm","Piano","Buffalo","Castle","Pepper","Emerald",
+    "Dragonfly","Notebook","Falconer","Comet","Grapefruit","Obsidian","Bridge","Coffee","Horizon","Marble",
+    "Skylight","Blueberry","Penguin","Harbor","Lantern","Ruby","Fox","Caravan","Topaz","Astronaut",
+    "Coconut","Compass","Wildflower","Sword","Meteor","Guitar","Sunflower","Wolf","Coral","Basil",
+    "Library","Shark","Pinecone","Sapphire","Engine","Palm","Canary","Thunder","Violin","Starlight",
+    "Kiwi","Crystal","Lighthouse","Forest","Violet","Snowflake","Camel","Copper","Circuit","Cherry",
+    "Whale","Diamond","Pasta","Photon","Bamboo","Mountain","Cedar","Anchor","Robot","Hawk",
+    "Avalon","Turtle","Fireplace","Clover","Traveler","Orchid","Spear","Nebula","Moose","Mirror",
+    "Honey","Atlas","Fountain","Canyon","Ranger","Vanilla","Eagle","Bronze","Garden","Voyager",
+    "Swan","Engine","Acorn","Turquoise","Panorama","Heron","Vanilla","Citadel","Lobster","Horizon",
+    "Barley","Tundra","Lanternfish","Caramel","Vortex","Anvil","Toucan","Velvet","Jigsaw","Harvester",
+    "Falconry","Sundial","Waffle","Badlands","Titan","Pecan","Mosaic","Barracuda","Crater","Festival",
+    "Walrus","Blueprint","Peppermill","Tamarin","Lagoon","Ivory","Workshop","Monarch","Raspberry","Summit",
+    "Courier","Windmill","Harvester","Meteorite","Treasure","Dandelion","Chimney","Lighthouse","Firewood","Seagull",
+    "Snowstorm","Blacksmith","Sailboat","Walnut","Pavilion","Pebbles","Macaw","Starfish","Crescent","Truffle",
+    "Cavern","Steam","Oak","Voyager","Firework","Gladiator","Meadow","Porcelain","Dragonfruit","Blueprints",
+    "Evergreen","Canal","Badminton","Village","Parachute","Marmot","Cliffside","Toffee","Hedgehog","Crossbow",
+    "Observatory","Chestnut","Gondola","Skyscraper","Alligator","Prism","Constellation","Buttercup","Artisan","Meerkat",
+    "Monsoon","Chameleon","Firestone","Parmesan","Boulder","Squirrel","Riptide","Hummingbird","Meadowlark","Windbreaker",
+    "Crescendo","Fjord","Dragonfly","Moonstone","Waterwheel","Sundrop","Camel","Whisper","Beacon","Sandstone",
+    "Penguin","Fireplace","Elm","Moonbeam","Snowdrift","Antler","Willow","Treeline","Woodland","Cottage",
+    "Raindrop","Canteen","Macadamia","Wildcat","Tractor","Hourglass","Dune","Ravenwood","Pinecone","Orbit",
+    "Honeycomb","Stonework","Everglade","Campfire","Caribou","Snowfall","Marigold","Workbench","Caterpillar","Northwind"
 ];
 
 
@@ -96,7 +105,7 @@ let passwordLenght = passwordLenghtSelector.value;
 let password = "";
 
 while (password.length < passwordLenght) {
-    let randomNumber = Math.floor(Math.random()*(299 - 0 + 1)) + 0 ;
+    let randomNumber = Math.floor(Math.random() * passwordWords.length) ;
     if ( (password + passwordWords[randomNumber]).length <= passwordLenght ){
         password = password + passwordWords[randomNumber];
     } 
